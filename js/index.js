03a200d4778a2b40bb38d43137d3943c8814debe
Christian Broms <cb@christianbroms.com>
2019-12-02 20:28:28 -0500

    save level scores and add header

--- js/index.js
+++ js/index.js
@@ -1,4 +1,59 @@
-localStorage.removeItem("gameScore");
+//localStorage.removeItem("gameScore");
+const id = gameInProgress();
 
-prepareLocalMem("azy");
-console.log(getAllScores());
+// redirect to index if the game is not started
+if (!id) {
+    window.location.href = "/";
+}
+
+// var to store the interval used to score
+let scoreTicker;
+
+// update the score every second
+const startPlay = () => {
+    // setup the mem for the level
+    prepareLocalMem(id, levelNum);
+
+    // setup header with score and reset button
+    const headerDiv = document.createElement("DIV");
+    headerDiv.id = "header";
+
+    const restartButton = document.createElement("BUTTON");
+    restartButton.id = "restart";
+    restartButton.innerText = "Restart Level";
+    restartButton.onclick = () => {
+        endPlay();
+        window.location.href = "/level-1/";
+    };
+
+    // create the div to show the score
+    const scoreDiv = document.createElement("DIV");
+    scoreDiv.id = "score";
+
+    headerDiv.appendChild(scoreDiv);
+    headerDiv.appendChild(restartButton);
+    document.body.insertBefore(headerDiv, document.body.childNodes[0] || null);
+
+    scoreTicker = window.setInterval(() => {
+        updateGameScore(1, id, levelNum);
+        currentScore = getGameScore(id);
+
+        // format the time
+        const minutes = Math.floor(currentScore / 60);
+        let seconds = currentScore - minutes * 60;
+        seconds = seconds.toString().length === 1 ? `0${seconds}` : seconds;
+
+        document.getElementById(
+            "score"
+        ).innerText = `Your time: ${minutes}:${seconds}`;
+    }, 1000);
+};
+
+const endPlay = () => {
+    window.clearInterval(scoreTicker);
+};
+
+startPlay();
+
+// prepareLocalMem("azy");
+// console.log(getAllScores());
--- js/scoring.js
+++ js/scoring.js
@@ -1,26 +1,55 @@
-const prepareLocalMem = game => {
+// check if a game is currently being played and return the game ID
+const gameInProgress = () => {
+    return localStorage.getItem("gameStarted");
+};
+
+// start a game by placing the game ID in memory
+const startGame = game => {
+    localStorage.setItem("gameStarted", game);
+};
+
+// remove the game ID from mem
+const endGame = () => {
+    localStorage.removeItem("gameStarted");
+};
+
+// prepare to score a game by placing a scoring variable in mem and setting the game
+// attribute to zero. if there's already a variable there, reset the current level only
+const prepareLocalMem = (game, level) => {
     let oldGameObj = localStorage.getItem("gameScore");
     if (!oldGameObj) {
         let gameObj = {};
-        gameObj[game] = 0;
+        gameObj[game] = [];
+        gameObj[game][level - 1] = 0;
         localStorage.setItem("gameScore", JSON.stringify(gameObj));
+    } else if (!oldGameObj[game]) {
+        oldGameObj = JSON.parse(oldGameObj);
+        oldGameObj[game] = [];
+        oldGameObj[game][level - 1] = 0;
+        localStorage.setItem("gameScore", JSON.stringify(oldGameObj));
     } else {
         oldGameObj = JSON.parse(oldGameObj);
-        oldGameObj[game] = 0;
+        oldGameObj[game][level - 1] = 0;
         localStorage.setItem("gameScore", JSON.stringify(oldGameObj));
     }
 };
 
+// get retrieve the game score from mem by getting the scoring var and getting
+// game ID attribute
 const getGameScore = game => {
-    return JSON.parse(localStorage.getItem("gameScore"))[game];
+    const levels = JSON.parse(localStorage.getItem("gameScore"))[game];
+    return levels.reduce((sum, val) => {
+        sum + val;
+    });
 };
 
 const getAllScores = () => {
     return JSON.parse(localStorage.getItem("gameScore"));
 };
 
-const updateGameScore = (newScore, game) => {
+// add to the game's current score
+const updateGameScore = (newScore, game, level) => {
     let gameObj = JSON.parse(localStorage.getItem("gameScore"));
-    gameObj[game] = newScore;
+    gameObj[game][level - 1] += newScore;
     localStorage.setItem("gameScore", JSON.stringify(gameObj));
 };
